917d7d00250bc2f6934e72047fc7235a1a20155f
Adriel Rengel <adrielrengel@gmail.com>
2015-12-01 20:31:35 -0200

    inicial

---
+++ src/main/java/br/univel/DsFactory.java
@@ -0,0 +1,12 @@
+package br.univel;
+
+import net.sf.jasperreports.engine.JRDataSource;
+
+public class DsFactory {
+	
+	public static JRDataSource create() {
+		
+		return new CustomDs();
+	}
+	
+}
